d09b1432101e82e8b1fc30ef0d0b9ac135cb5595
RSepulvedaDev10 <94391613+RSepulvedaDev10@users.noreply.github.com>
2022-01-03 10:30:57 -0600

    Update Dev10 M07 - Lambda and MapReduce Exercise.py

--- Dev10 M07 - Lambda and MapReduce Exercise.py
+++ Dev10 M07 - Lambda and MapReduce Exercise.py
@@ -1,3 +1,10 @@
 import csv
 
+data = list()
+
 with open('911_Calls_for_Service_(Last_30_Days).csv') as csvFile:
+    csvReader = csv.DictReader(csvFile)
+    for line in csvReader:
+        data.append(line)
+        
+print(data)
